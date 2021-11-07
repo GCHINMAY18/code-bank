51ab9822f34ae01823962b2a6fabd82c3f55e989
GCHINMAY18 <93787895+GCHINMAY18@users.noreply.github.com>
2021-11-07 19:55:57 +0530

    Add files via upload

---
+++ one.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("entre the number a\n");
+scanf("%d",&a);
+
+printf("entre the number b\n");
+scanf("%d",&b);
+
+printf("entre the number c\n");
+scanf("%d",&c);
+
+printf("calculation of the desired result is %d" , a+b-c);
+
+return 0;
+
+}
