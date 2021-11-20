867ee784067017ee5ed11568061b6a623d413de6
GCHINMAY18 <93787895+GCHINMAY18@users.noreply.github.com>
2021-11-20 19:16:01 +0530

    with important point of exp2 in comments

---
+++ tutorial15_for_loop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i, j;
+    for ( i =0, j=0 ; j<14,i<9 ; i++ ,j++)                 //example of more than more variable in exprassion one 
+                     
+                //the case which we have taken the expression 2
+                //   at last will act the condition for the loop to be terminate        
+
+    {
+        printf("%d %d\n" , i,j );
+    }
+
+
+    return 0;
+}
