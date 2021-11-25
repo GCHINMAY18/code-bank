0bda1ef29f8dccd75a65a00e45a99bce94007a9e
GCHINMAY18 <93787895+GCHINMAY18@users.noreply.github.com>
2021-11-25 22:44:36 +0530

    Update practice1.c

--- practice1.c
+++ practice1.c
@@ -6,17 +6,25 @@ int main()
     printf("Entre a number\n");
     scanf("%f",&num );
 
-     if (num<=0.0)
+     if (num<0.0)
      {
         printf("The number %0.1f which you have   entered is negative ",num);
      }
     
-    else if (num>=0.0)
+    else if (num>0.0)
     {
         printf("The number %0.1f which you have   entered is positive" , num);
 
     }
-
+     else if (num==0.0){                                                     //use double equal to for comparison because single equalto is assignmeant operator
+         printf("The number is zero ");
+     }
+    
+    else{
+    printf("Please entre a valid number");
+    }
+         
+        
      
 
     return 0;
