6a5f52fa432f37adca4a328561c385334253a599
GCHINMAY18 <93787895+GCHINMAY18@users.noreply.github.com>
2021-11-26 17:21:32 +0530

    Add files via upload

---
+++ lab2.c
@@ -0,0 +1,88 @@
+#include<stdio.h>
+#include<math.h>
+
+/*int main()
+{
+    int c,f;
+    printf("Entre the value of temp in celcius  \n");
+    scanf("%d",&c);
+
+    printf("Entre the value of temp in farnihite  \n");
+    scanf("%d" , &f);
+
+
+    printf("celcius to farnihite is:%d", c= (f-32)*5/9);
+
+    
+    return 0;
+}
+*/
+
+
+/*int main()
+{
+    int x1,x2,y1,y2;
+    printf("Entre the value of x1,x2,y1,y2 respectively\n");
+    scanf("%d",&x1);
+    scanf("%d",&x2);
+    scanf("%d",&y1);
+    scanf("%d",&y2);
+     
+
+     int  a=(x1-x2)^2;
+    int   b=(y1-y2)^2;
+     int dis= sqrt(a-b);
+
+     printf(" The value of distance is :%d" , dis);
+
+    return 0;
+}
+*/
+
+
+
+/*int main(int argc, char const *argv[])
+{
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+
+       
+     printf(" original value of a:%d\n",a);
+     printf("original value of b:%d\n",b);                    ye tanmay ne bataya tha 
+
+     int temp = a;
+     a = b;
+     b = temp;
+
+     printf("Temp value of a:%d\n",a);
+     printf("Temp value of b:%d",b);
+ 
+    
+    return 0;
+}
+*/
+
+int main()
+{
+    int a,b;
+    printf("Entre the value of a:");
+    scanf("%d",&a);
+    printf("Entre the value of b:");
+    scanf("%d\n",&b);
+
+     printf(" original value of a:%d\n",a);
+     printf("original value of b:%d\n",b); 
+
+     int temp=a;
+     a=b;
+     b=temp;
+
+       printf("Temp value of a:%d\n",a);
+     printf("Temp value of b:%d",b);
+
+
+
+
+    return 0;
+}
