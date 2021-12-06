7163bb71be4d822f85345fe21e654e2c6272a5a1
GCHINMAY18 <93787895+GCHINMAY18@users.noreply.github.com>
2021-12-06 22:52:12 +0530

    factorial example

---
+++ tutorial21_recursion.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/*int factorial(int number)
+{
+    if (number == 1 || number == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return (number * factorial(number - 1));
+    }
+}
+
+int main()
+{
+     int num;
+     printf("Entre the number for which you want factorial of that\n");
+     scanf("%d",&num);
+     printf("The factorial of the number :%d \n Is :%d " , num, factorial(num));
+    return 0;
+}
+*/
+
+
+int main()
+{
+    int n ,i;
+    printf("Entre the number \n");
+    scanf("%d",&n);
+     int multi=1;
+     for ( i = 1; i <=n; i++)
+     {
+        multi=multi*i;
+     }
+     printf("The factorial of the number is %d",multi);
+    
+    return 0;
+}
+
+
+
