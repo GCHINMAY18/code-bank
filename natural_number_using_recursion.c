27584faf76b0cb4a7d6603b928378766962f7d42
GCHINMAY18 <93787895+GCHINMAY18@users.noreply.github.com>
2021-12-07 22:40:48 +0530

    Add files via upload

---
+++ natural_number_using_recursion.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int natural(int n)
+{
+    if (n)
+    {
+        natural(n-1);
+    }
+    
+    else
+    {
+        return n;
+    }
+
+    printf("%-3d",n);
+}
+
+
+int main()
+{
+     int num;
+    printf("Entre the natural number till where you want natural number  \n");
+    scanf("%d",&num);
+
+    natural(num);
+    
+    return 0;
+}
